9452f9ce5a8f56180818348d4c8d942482ecbb34
aidan020 <9029086@student.zadkine.nl>
2026-06-19 10:03:59 +0200

    Update quiz.php

--- wordpress/thema-ai/quiz.php
+++ wordpress/thema-ai/quiz.php
@@ -1,10 +1,10 @@
-<!DOCTYP<!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="nl">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Quiz - AI Nepnieuws Info</title>
-  <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/style.css">
+   <link rel="stylesheet" href="../style/style.css">
 </head>
 <body>
 <?php get_header(); ?>
